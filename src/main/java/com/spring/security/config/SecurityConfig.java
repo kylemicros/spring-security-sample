920c5acb12b319cb76fffadc538ac7ea31482df9
kylemicros <kyle.marlowe.pacana.moreno@gmail.com>
2024-09-17 15:59:34 +0800

    Added Content Security Police and XSS Mitigation

--- src/main/java/com/spring/security/config/SecurityConfig.java
+++ src/main/java/com/spring/security/config/SecurityConfig.java
@@ -16,6 +16,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.header.writers.StaticHeadersWriter;
+import org.springframework.security.web.header.writers.XXssProtectionHeaderWriter;
 
 import com.spring.security.config.jwt.AuthEntryPointJwt;
 import com.spring.security.config.jwt.AuthTokenFilter;
@@ -52,6 +54,8 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		String REPORT_TO = "{\"group\":\"csp-violation-report\",\"max_age\":2592000,\"endpoints\":[{\"url\":\"https://localhost:8888/report\"}]}";
+
 		http
 				.csrf((csrf) -> csrf.disable())
 				.authorizeHttpRequests((authz) -> authz
@@ -61,7 +65,18 @@ public class SecurityConfig {
 				.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.exceptionHandling((exc) -> exc
 						.authenticationEntryPoint(entryPointJwt));
+
+		http
+				.headers((headers) -> headers
+						.addHeaderWriter(new StaticHeadersWriter("Report-To", REPORT_TO))
+						.xssProtection(
+								(xss) -> xss.headerValue(XXssProtectionHeaderWriter.HeaderValue.ENABLED_MODE_BLOCK))
+						.contentSecurityPolicy(
+								(cps) -> cps.policyDirectives(
+										"style-src 'self' somecdn.css.com; script-src 'self'; form-action 'self'; report-uri /report; report-to csp-violation-report")));
+
 		http.authenticationProvider(authenticationProvider());
+
 		http.addFilterBefore(authTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
