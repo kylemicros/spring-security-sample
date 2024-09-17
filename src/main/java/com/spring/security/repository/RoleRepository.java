75cf8ba983c9407c4275c095d8733630ce7da7e0
kylemicros <kyle.marlowe.pacana.moreno@gmail.com>
2024-09-17 17:11:31 +0800

    SQL injection config

--- src/main/java/com/spring/security/repository/RoleRepository.java
+++ src/main/java/com/spring/security/repository/RoleRepository.java
@@ -3,6 +3,7 @@ package com.spring.security.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.spring.security.model.RoleId;
@@ -10,5 +11,7 @@ import com.spring.security.model.enums.Roles;
 
 @Repository
 public interface RoleRepository extends JpaRepository<RoleId, Long> {
+
+	@Query("SELECT r FROM RoleId r WHERE r.roles = :roles")
 	Optional<RoleId> findByRoles(Roles roles);
 }
