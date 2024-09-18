42e8940419ad0c0ce8867bf3ee9b9c046ddc447e
kylemicros <kyle.marlowe.pacana.moreno@gmail.com>
2024-09-18 11:21:06 +0800

    Updated transactional delete by ID

--- src/main/java/com/spring/security/config/service/RefreshTokenService.java
+++ src/main/java/com/spring/security/config/service/RefreshTokenService.java
@@ -4,6 +4,8 @@ import java.time.Instant;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,8 +15,12 @@ import com.spring.security.repository.TokenRepository;
 import com.spring.security.repository.UserRepository;
 import com.spring.security.service.exception.TokenRefreshException;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class RefreshTokenService {
+	private static final Logger logger = LoggerFactory.getLogger(RefreshTokenService.class);
+
 	@Value("${security.jwt.refresh-token-expiration}")
 	private Long refreshTokenDurationMs;
 
@@ -52,7 +58,9 @@ public class RefreshTokenService {
 		return token;
 	}
 
+	@Transactional
 	public Long deleteByUserId(Long id) {
+		logger.info("Attempting to delete token...");
 		return tokenRepository.deleteByUser(userRepository.findById(id).get());
 	}
 }
